2523e79365fe89b5de47fabf52c4e102d5c6966f
Gussatt <gustavoasaturnino@gmail.com>
2026-05-19 14:58:56 -0300

    style: migrate to tailwind v4 and define dracula theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,26 +6,7 @@ module.exports = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      colors: {
-        dracula: {
-          bg: '#282a36',
-          fg: '#f8f8f2',
-          selection: '#44475a',
-          comment: '#6272a4',
-          cyan: '#8be9fd',
-          green: '#50fa7b',
-          orange: '#ffb86c',
-          pink: '#ff79c6',
-          purple: '#bd93f9',
-          red: '#ff5555',
-          yellow: '#f1fa8c',
-        },
-      },
-      fontFamily: {
-        mono: ['var(--font-mono)', 'monospace'],
-      },
-    },
+    extend: {},
   },
   plugins: [],
 }
