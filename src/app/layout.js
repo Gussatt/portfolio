b100eb30cc6dfd98cc0f8602d2c72e9027ecb6b7
Gussatt <gustavoasaturnino@gmail.com>
2026-05-20 01:04:52 -0300

    style: update titles to personal greeting with translations

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,7 +4,7 @@ import './globals.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Gustavo Saturnino',
+  title: "Hi, I'm Gustavo",
   description: 'Currently running multiple interests and working as a Support Engineer',
 }
 
