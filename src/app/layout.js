a34d0fa0590f2713ea9c47e21dcaf9b4745417c5
Gussatt <gustavoasaturnino@gmail.com>
2026-05-20 01:03:32 -0300

    style: update metadata description

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,7 +5,7 @@ const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Gustavo Saturnino',
-  description: 'Technical Support Specialist and Software Engineer Portfolio',
+  description: 'Currently running multiple interests and working as a Support Engineer',
 }
 
 export default function RootLayout({ children }) {
